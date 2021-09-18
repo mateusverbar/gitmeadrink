22e8fc4c283727f40c2132d5f079a5f4fc4d7163
Breania Harvey <harvey.breania@gmail.com>
2021-09-17 21:01:20 -0500

    shuffle function added

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # gitmeadrink
+
+## USER STORY
+
+AS a user
+I WANT an app that allows me to put in my favorite type of alcohol and returns a random list of cocktails including the selected alcohol
+SO THAT I can try new drinks with my favorite type of alcohol spirit
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+// variable to hold user alcohol selection
+// variable to reference the ul div
+const recipesEl = document.querySelector(".recipes");
+
+function shuffle(arr) {
+  arr = arr.sort(() => Math.random() - 0.5)
+  arr = arr.slice(0, 20)
+  return arr;
+}
+
+// variable to create li element to append to ul element
 fetch(
     'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin'
   )
@@ -6,18 +17,31 @@ fetch(
     })
     .then(function(ingredientResponse) {
       console.log(ingredientResponse);
-      // for (let i = 0; i < ingredientResponse.drinks.length; i++) 
+      const drinkList = ingredientResponse.drinks
+      let randomDrinks = shuffle(drinkList);
+      console.log(randomDrinks);
+      
+      // drinkId needs to be added as a class to each image
       const drinkId = ingredientResponse.drinks[0].idDrink
-      console.log(drinkId);
-      return fetch("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + drinkId);
-    })
-    .then(function(drinkDetails) {
-      return drinkDetails.json();
-    })
-    .then(function(drinkDetails) {
-      console.log(drinkDetails);
-    })
-    ;
+      // the drink picture will be posted on the page
+      const drinkPicture = ingredientResponse.drinks[0].strDrinkThumb
+      // drink name will be added to the span via textContent
+      const drinkName = ingredientResponse.drinks[0].strDrink
+      // thumbnail needs to print on page, drink id should be id for image
+
+
+      // post the thumbnail to page
+  // use 2nd fetch request details about selected drink
+  // random 20 thumbnails on page; add link asking user if they would like to load another random 20
+    //   return fetch("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + drinkId);
+    // })
+    // .then(function(drinkDetails) {
+    //   return drinkDetails.json();
+    // })
+    // .then(function(drinkDetails) {
+    //   console.log(drinkDetails);
+     })
+    // ;
 // fetch to get drink id for drinks that match selected ingredient
 // fetch to take the drink id and pass back cocktail recipes
 // display the list of recipes in the DOM
