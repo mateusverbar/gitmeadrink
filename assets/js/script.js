1065cc5036123325838e52d77d3fd21414b131ab
Matthew Barros <matthewbarros@Matthews-MacBook-Pro.local>
2021-09-20 23:14:08 -0500

    drink-image-list-done

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,6 @@
 // variable to reference the ul div
 const recipesEl = document.querySelector(".recipes");
 
-function eventListenerHandler() {
-  let drinkClass = document.querySelectorAll(".drink");
-  // let drinkArray = Array.prototype.slice.call(drinkClass);
-  console.log(drinkClass);
-  // console.log(drinkArray);
-  for(let i = 0; i < drinkClass.length; i++) {
-    console.log(drinkClass[i]);
-    drinkClass[i].addEventListener("click", displayRecipe());
-  }
-}
-
 function shuffle(arr) {
   arr = arr.sort(() => Math.random() - 0.5)
   arr = arr.slice(0, 20)
@@ -68,7 +57,62 @@ function displayDrink() {
 
 function displayRecipe(drinkId) {
   console.log(drinkId);
+  fetch("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + drinkId)
+  .then(function(drinkDetails) {
+    return drinkDetails.json()
+  })
+  .then(function(drinkDetails) {
+    console.log(drinkDetails);
+    drinkName = drinkDetails.drinks[0].strDrink;
+    console.log(drinkName);
+
+    ingredient = drinkDetails.drinks[0].strIngredient1;
+    console.log(ingredient);
+
+    measurement = drinkDetails.drinks[0].strMeasure1;
+    console.log(measurement);
+
+    document.querySelector(".modal-body").innerHTML=`${ingredient}:  ${measurement}`;
+
+    $("#drink-form-modal").modal("show")
+
+    //push all the ingredients into an array
+    //loop through the array to return an ingredient in the modal if not null
+
+    // modal was triggered
+  // $("#drink-form-modal").on("shown.bs.modal", function() {
+  //   $("#drink-content").trigger('focus');
+  //   document.querySelector(".modal-body").innerHTML=ingredient;
+  // clear values
+  //$("#modal-title, #modal-body").val("");
+
+  // modal was triggered
+  // $("#drink-form-modal").on("show.bs.modal", function() {
+  // //clear values 
+  // $("#modal-title, #modal-body").val("");
+  // });
+  // //modal is fully visible
+  // $("#drink-form-modal").on("shown.bs.modal", function() {
+  //   document.querySelector(".modal-body").innerHTML=ingredient;
+  // })
+
+  });
+
 }
+
+    // let drinkName = drinkDetails.strDrink
+    // console.log(drinkName);
+
+    // let modalEl = document.createElement("div");
+    // modalEl.classList.add("modal");
+    // modalEl.setAttribute("id","drinkModal");
+
+    // let modalContentEl = document.createElement("div");
+    // modalContentEl.classList.add("modal-content");
+
+
+    // modalContent.innerHTML(`${drinkDetails}`)
+
 
 
     // post the thumbnail to page
