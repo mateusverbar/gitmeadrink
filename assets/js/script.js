c762e3693f213bf19fac1f704a3c9b1fc6317033
Breania Harvey <harvey.breania@gmail.com>
2021-09-21 18:04:33 -0500

    pseudocode added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-// variable to hold user alcohol selection
-// variable to reference the ul div
+// variable to grab the recipes div
 const recipesEl = document.querySelector(".recipes");
 
+// function is used to shuffle the order of an array
 function shuffle(arr) {
   arr = arr.sort(() => Math.random() - 0.5)
   arr = arr.slice(0, 20)
@@ -11,12 +11,17 @@ function shuffle(arr) {
 
 function displayDrink() {
   
+  // grabs the users alcohol selection
   let alcoholType = document.querySelector("#alcohol-type").value;
   let recipeNameEl = document.querySelector("#recipe-name");
+
+  // add the users selection in the recipe name span to verify user selection
   recipeNameEl.textContent=`${alcoholType}!`;
 
+  // sets the recipe div to empty
   recipesEl.textContent = "";
-  // variable to create li element to append to ul element
+
+  // pass the users alcohol selection into the fetch
   fetch(
       `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${alcoholType}`
     )
@@ -26,176 +31,131 @@ function displayDrink() {
       .then(function(ingredientResponse) {
         console.log(ingredientResponse);
         const drinkList = ingredientResponse.drinks
+
+        // shuffle drinks in the array
         let randomDrinks = shuffle(drinkList);
         console.log(randomDrinks);
+
 
         for (let i = 0; i < randomDrinks.length; i++) {
           let drinkId = randomDrinks[i].idDrink
           let drinkName = randomDrinks[i].strDrink
           let drinkPicture = randomDrinks[i].strDrinkThumb
 
+          // create a flex div to hold all the drink images
           let drinkDivEl = document.createElement("div")
           drinkDivEl.classList.add("flex-item");
 
+          // create the element for the drink and set it to name
           let drinkTitleEl = document.createElement("span");
           drinkTitleEl.innerHTML = 
             `<h4>${drinkName}</h4>`
 
+          // create an image element for each of the drinks in the random drinks array
           let drinkImageEl = document.createElement("img");
           drinkImageEl.classList.add("drink");
           drinkImageEl.setAttribute("id", `${drinkId}`);
           drinkImageEl.setAttribute("alt", `${drinkName}`);
           drinkImageEl.setAttribute("src", `${drinkPicture}`);
 
-          drinkDivEl.addEventListener("click", ()=>displayRecipe(drinkId));//changed drinkImgEl to drinkDivEl so if they click anywhere in the div
+          // add a click event to the drinkDivEl that will run the displayRecipe function as a call back function and pass the drinkId on where called
+          drinkDivEl.addEventListener("click", ()=>displayRecipe(drinkId));
 
           recipesEl.appendChild(drinkDivEl)
           drinkDivEl.appendChild(drinkTitleEl);
           drinkDivEl.appendChild(drinkImageEl)
 
-          //create the button 
-          // <button type="button" id="refresh" class="alcohol-type">Refresh List</button>
-
           
         }
-        // var refreshBtn = document.createElement("button");
+        // create a refresh button to append after all the pictures
+        var refreshBtn = document.createElement("button");
           
-        //   refreshBtn.setAttribute("type","button");
-        //   refreshBtn.setAttribute("id","refresh");
-        //   refreshBtn.classList.add("alcohol-type","w-100");
-        //   refreshBtn.textContent="Refresh List";
+        refreshBtn.setAttribute("type","button");
+        refreshBtn.setAttribute("id","refresh");
+        refreshBtn.classList.add("alcohol-type","w-100");
+        refreshBtn.textContent="Refresh List";
 
-        //   document.querySelector(".flex-container").appendChild(refreshBtn);
+        document.querySelector(".flex-container").appendChild(refreshBtn);
+
+        // add a click event to the refresh button to rerun the displayDrink function to display new drinks on the page
+        document.querySelector("#refresh").addEventListener("click",displayDrink);
       })
 }
 
 function displayRecipe(drinkId) {
   console.log(drinkId);
-  document.querySelector(".modal-body").innerHTML="";// clears out modal content on new selection
-  fetch("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + drinkId)
+  
+  // clears out modal content on new selection
+  document.querySelector(".modal-body").innerHTML="";
+
+  // pass the drinkId to the 2nd fetch to retreive details about the drink
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`)
   .then(function(drinkDetails) {
     return drinkDetails.json()
   })
   .then(function(drinkDetails) {
     console.log(drinkDetails.drinks[0]);
-
-    //ingredients, amounts, and instructions
     
+    // pass the array from the response to the getMetheingredients function
     getMetheIngredients(drinkDetails.drinks[0]);
-
-    //ingredient = drinkDetails.drinks[0].strIngredient1;
-    //console.log(ingredient);
-
-    //measurement = drinkDetails.drinks[0].strMeasure1;
-    //console.log(measure);
-
-    // document.querySelector(".modal-body").innerHTML=`${ingredient}:  ${measure}`;
-    // document.querySelector(".modal-title").textContent=drinkName;
-    // $("#drink-form-modal").modal("show")
-
-    //push all the ingredients into an array
-    //loop through the array to return an ingredient in the modal if not null
-
-    // modal was triggered
-  // $("#drink-form-modal").on("shown.bs.modal", function() {
-  //   $("#drink-content").trigger('focus');
-  //   document.querySelector(".modal-body").innerHTML=ingredient;
-  // clear values
-  //$("#modal-title, #modal-body").val("");
-
-  // modal was triggered
-  // $("#drink-form-modal").on("show.bs.modal", function() {
-  // //clear values 
-  // $("#modal-title, #modal-body").val("");
-  // });
-  // //modal is fully visible
-  // $("#drink-form-modal").on("shown.bs.modal", function() {
-  //   document.querySelector(".modal-body").innerHTML=ingredient;
-  // })
 
   });
 }
 
 
 function getMetheIngredients(obj) {
-  for (let i = 1; i < 20; i++) {      //Thanks Rommel for this for-loop!
+
+  var ingredientListEl = document.createElement("ul");
+  var ingredientTitle = document.createElement("h5");
+
+  // append the ul element to the modal body
+  document.querySelector(".modal-body").appendChild(ingredientListEl);
+  // append the title for the ingredients to the ul element
+  ingredientListEl.appendChild(ingredientTitle);
+
+  for (let i = 1; i < 20; i++) { 
+    // if the value of strIngredient is null ignore it
     if (!obj[`strIngredient${i}`]) {
       break;
       } else {
+      // otherwise grab the specified information
       let ingredient = obj[`strIngredient${i}`];
       let measure = obj[`strMeasure${i}`];
       let instructions = obj[`strInstructions`];
       console.log(ingredient);
       console.log(measure);
       console.log(instructions);
-
+    
       drinkName = obj[`strDrink`];
       console.log(drinkName);
 
-      var ingredientListEl = document.createElement("ul");
+      // create the li element to hold the ingredients
       var ingredientListItemEl = document.createElement("li");
-      var ingredientTitle = document.createElement("h5");
-      ingredientTitle.textContent="Ingredients";
 
+      // sets ingredient title text
+      ingredientTitle.textContent="Ingredients:";
+      
+      // creates a title element for the instructions
       var instructionsTitle = document.createElement("h5");
-      instructionsTitle.textContent="Instructions";
+      instructionsTitle.textContent="Instructions:";
       var instructionsEl = document.createElement("p");
 
+      // sets the element html to the instructions from the response
       instructionsEl.innerHTML=instructions;
       
+      // sets the li item to the ingredient and measurement for that ingredient
       ingredientListItemEl.innerHTML=`${ingredient}:  ${measure}`;
-
-      document.querySelector(".modal-body").appendChild(ingredientListEl);
+      
+      // then append the ingredients list to the ul element
       ingredientListEl.appendChild(ingredientListItemEl);
       
-      //document.querySelector(".modal-body").innerHTML=`${ingredient}:  ${measure}`;
-      //document.querySelector(".modal-body").innerHTML="<h1>" + ingredient + ":</h1>";
+      // sets the drink name in the modal header
       document.querySelector(".modal-title").textContent=drinkName;
       $("#drink-form-modal").modal("show")
       }
   }
 
-  ingredientListEl.appendChild(ingredientTitle);
-
-
   document.querySelector(".modal-body").appendChild(instructionsTitle);
-  document.querySelector(".modal-body").appendChild(instructionsEl); //This one has to be here otherwise you get the instructions printed after each list item
+   //This one has to be here otherwise you get the instructions printed after each list item
+  document.querySelector(".modal-body").appendChild(instructionsEl);
 };
-
-
-
-
-
-
-    // let drinkName = drinkDetails.strDrink
-    // console.log(drinkName);
-
-    // let modalEl = document.createElement("div");
-    // modalEl.classList.add("modal");
-    // modalEl.setAttribute("id","drinkModal");
-
-    // let modalContentEl = document.createElement("div");
-    // modalContentEl.classList.add("modal-content");
-
-
-    // modalContent.innerHTML(`${drinkDetails}`)
-
-
-
-    // post the thumbnail to page
-  // use 2nd fetch request details about selected drink
-  // random 20 thumbnails on page; add link asking user if they would like to load another random 20
-    //   return fetch("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + drinkId);
-    // })
-    // .then(function(drinkDetails) {
-    //   return drinkDetails.json();
-    // })
-    // .then(function(drinkDetails) {
-    //   console.log(drinkDetails);
-    // ;
-// fetch to get drink id for drinks that match selected ingredient
-// fetch to take the drink id and pass back cocktail recipes
-// display the list of recipes in the DOM
-// optional clickable drink thumbnail to take them to the drink website in another webpage
-
-document.querySelector("#refresh").addEventListener("click",displayDrink);
